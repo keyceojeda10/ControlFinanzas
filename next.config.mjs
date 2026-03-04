fc2aca0ed50afbbc504144d3a33ee9ec0bb89e01
keyceojeda10 <keyceojeda10@gmail.com>
2026-03-04 10:18:51 -0500

    perf: optimizar next.js y agregar índices a la base de datos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ['@/components/ui', '@/lib'],
+  },
 };
 
 export default nextConfig;
