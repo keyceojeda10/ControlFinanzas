2f4e42e414624f7691eff71d4f857f0d4998ff9f
keyceojeda10 <ccaojd@gmail.com>
2026-03-03 23:45:02 -0500

    Fix hydration warning from browser extensions

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={`${geistSans.variable} antialiased`}>
         <SessionProvider>
           {children}
